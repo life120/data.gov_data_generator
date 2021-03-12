4409dae906139038dec870a282fd19bcd8248f29
life120 <69447142+life120@users.noreply.github.com>
2021-03-12 11:44:39 +0800

    End Message after end code run

--- index.py
+++ index.py
@@ -4,15 +4,17 @@ import json as json
 import urllib3 as urllib3
 import requests
 
-##code for NEA weather code duh
+##code for NEA weather code
 url = 'https://api.data.gov.sg/v1/environment/wind-direction?date=2020-10-10'
 url2 = 'https://api.data.gov.sg/v1/environment/wind-direction?date_time=2020-10-10T20%3A00%3A00&date=2020-10-10'
 
 data = requests.get(url).json()
 # df = pd.DataFrame.from_dict(data)
 print(data)
-# print(data['items'][0].get('readings'))
-# list1 = data['items'][0].get('readings')
-# for item in list1:
-#     if item.get('station_id')== 'S109':
-#         print(item.get('value'))
+print(data['items'][0].get('readings'))
+list1 = data['items'][0].get('readings')
+for item in list1:
+    if item.get('station_id')== 'S109':
+        print(item.get('value'))
+
+print("Completed")
