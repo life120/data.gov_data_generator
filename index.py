8f9501f5a7c35dcc823f8d82c05c5c3bc5b59019
life120 <69447142+life120@users.noreply.github.com>
2021-03-12 13:22:23 +0800

    Deciphering Metadata portion

--- index.py
+++ index.py
@@ -12,10 +12,16 @@ data = requests.get(url).json()
 # df = pd.DataFrame.from_dict(data)
 # print(data)
 # print(data['items'][0].get('readings'))
-list1 = data['items'][0].get('readings')
-for item in list1:
-    if item.get('station_id')== 'S109':
-        print(item.get('value'))
-        print('\n')
+list1 = data['items'][0]
+list2 = data['items'][1]
+# for item in list1:
+#     if item.get('station_id')== 'S109':
+#         print(item.get('value'))
+#         print('\n')
 #added a new line
+# print(list1.get('timestamp'))
+# print(list2.get('timestamp'))
+for item in data['metadata']['stations']:
+    print('ID is ' + str(item['id']) + '. It is located at ' + item['name'])
+
 print("Completed")
