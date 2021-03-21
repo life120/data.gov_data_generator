81bc4d663da26a40c4229f70a598011d16269333
life120 <69447142+life120@users.noreply.github.com>
2021-03-21 13:27:35 +0800

    Save file and data edit updated
    Timestamp validation updated to detect missing timestamp and input missing timestamp with interpolated data.
    
    Save file summarises the missing timestamp and lists the range of dates missing.

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ station_id = ""
 date = ""
 first_timestamp = "YYYY-MM-DDT00:00:00+08:00"
 last_timestamp = "23:59:00"
+missing_timestamp = []
 
 ##code for NEA weather code
 # url = 'https://api.data.gov.sg/v1/environment/wind-direction?date=2020-10-10'
@@ -47,9 +48,14 @@ def newTimeStamp(currentTimeStamp, time):
         tempTimeStamp = part1 + newHrStr + newMinStr + part4
         return tempTimeStamp
 
+def firstTimeStamp(currentTimeStamp):
+    part1 = currentTimeStamp[:11]
+    part2 = currentTimeStamp[11:13]
+    part4 = currentTimeStamp[16:]
+    newMinStr = ':00'
+    tempTimeStamp = part1 + part2 + newMinStr + part4
+    return tempTimeStamp
 ##Prints out the time stamp and the data from station_id input
-
-
 
 date = input("Please indicate the date that you want to draw the wind direction data from(YYYY-MM-DD): ")
 url = url + date
@@ -66,54 +72,48 @@ timeStampCount = 0
 timeStampNoCount = 0
 with open(fileName,'w') as myFile:
     for item in data['items']:
+        timeStamp = item["timestamp"]
+        readings = item['readings']
         if count == 0:
-            timeStamp = first_timestamp
-            station_value = item['value']
-            myFile.write(timeStamp + ";" + station_id)
-            count = count + 1
-
-        if count > 0:
-            timeStamp = item['timeStamp']
-            station_value = item['value']
-            currentTimeStamp = timeConvert(timeStamp[11:16])
-            previousTimeStamp = timeConvert(previous_timestamp[11:16])
-            timeDiff = currentTimeStamp - previousTimeStamp
-            station_value = str(item['value'])
-            if timeDiff > 1:
-                timeStampNoCount = timeStampNoCount + timeDiff - 1
-                for time in range(1,timeDiff):
-                    tempTimeStamp = newTimeStamp(previous_timestamp, time)
-                    print("At {}, readings at {} = {}\n".format(tempTimeStamp, station_id, station_value))
-            
-            print("At {}, readings at {} = {}\n".format(timeStamp, station_id, station_value))
-            previous_timestamp = timeStamp
-            timeStampCount = timeStampCount + 1
-            timeDiff = 0
-
-            timeStamp = item['timestamp']
-            currentTimeStamp = timeConvert(timeStamp[11:16])
-            previousTimeStamp = timeConvert(previous_timestamp[11:16])
-            timeDiff = currentTimeStamp - previousTimeStamp
-            if timeDiff > 1:
-                timeStampNoCount = timeStampNoCount + timeDiff - 1
-
-            readings = item['readings']
+            temTimeStamp = firstTimeStamp(timeStamp)
             for item in readings:
                 if item['station_id'] == station_id:
                     station_value = str(item['value'])
             if station_value == "":
-                station_value = 0
-                count = count + 1
-            # print("At {}, readings at {} = {}".format(timeStamp, station_id, station_value))
-            myFile.write("At {}, readings at {} = {}\n".format(timeStamp, station_id, station_value))
+                station_value = str(0)
+            myFile.write(temTimeStamp + ";" + station_value + "\n")
+            count = count + 1
+
+        if count > 0:
+            count = count + 1
+            currentTimeStamp = timeConvert(timeStamp[11:16])
+            previousTimeStamp = timeConvert(previous_timestamp[11:16])
+            timeDiff = currentTimeStamp - previousTimeStamp
+            for item in readings:
+                if item['station_id'] == station_id:
+                    station_value = str(item['value'])
+            if station_value == "":
+                station_value = str(0)
+            
+            if timeDiff > 1:
+                timeStampNoCount = timeStampNoCount + timeDiff - 1
+                missing_timestamp.append(timeStamp)
+                for time in range(1,timeDiff):
+                    tempTimeStamp = newTimeStamp(previous_timestamp, time)
+                    myFile.write(tempTimeStamp + ";" + station_value + "\n")
+            
+            myFile.write(timeStamp + ";" + station_value + "\n")
             previous_timestamp = timeStamp
             timeStampCount = timeStampCount + 1
             timeDiff = 0
-    # print('There are {} empty slots for the day.'.format(str(count)))
-    timeStampNoCount = timeStampNoCount - 1 
-    myFile.write('There are {} empty slots for the day.\nThere are {} lines.\nThere are {} missing lines'.format(str(count), str(timeStampCount), str(timeStampNoCount)))  
+
+    myFile.write('There are {} slots for the day.\nThere are {} lines.\nThere are {} missing lines'.format(str(count), str(timeStampCount), str(timeStampNoCount)))  
     myFile.write('\n' + str(1438 - timeStampCount))
+    myFile.write('\nTimestamp missing in the list are listed below:\n')
+    for item in missing_timestamp:
+        myFile.write(item + "\n")
 
 print('There are {} empty slots for the day.\nThere are {} lines.\nThere are {} missing lines'.format(str(count), str(timeStampCount), str(timeStampNoCount)))  
 print('\n' + str(1438 - timeStampCount))
 print("Completed")
+print(missing_timestamp)
