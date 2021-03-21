4bf04677e11e6e011df7178440ab40625a9f5847
life120 <69447142+life120@users.noreply.github.com>
2021-03-21 11:48:36 +0800

    Compiling test and master

--- index.py
+++ index.py
@@ -4,20 +4,17 @@ import json as json
 import urllib3 as urllib3
 import requests
 
+count = 0
+station_value = ""
+station_id = ""
+date = ""
+first_timestamp = "YYYY-MM-DDT00:00:00+08:00"
+last_timestamp = "23:59:00"
+
 ##code for NEA weather code
 # url = 'https://api.data.gov.sg/v1/environment/wind-direction?date=2020-10-10'
 url = 'https://api.data.gov.sg/v1/environment/wind-direction?date='
 url2 = 'https://api.data.gov.sg/v1/environment/wind-direction?date_time=2020-10-10T20%3A00%3A00&date=2020-10-10'
-
-# data = requests.get(url).json()
-
-##This prints out the number of headers in the json file
-# for item in data:
-#     print(item)
-
-##this finds out the ID and name of each station
-# for item in data['metadata']['stations']:
-#     print('ID is ' + str(item['id']) + '. It is located at ' + item['name'])
 
 def timeConvert(timeStamp):
     min = int(timeStamp[3:])
@@ -25,16 +22,39 @@ def timeConvert(timeStamp):
     time = 60*hr + min
     return time
 
+def newTimeStamp(currentTimeStamp, time):
+    part1 = currentTimeStamp[:11]
+    part2 = currentTimeStamp[11:13]
+    part4 = currentTimeStamp[16:]
+    oldMin = int(currentTimeStamp[14:16])
+    newMin = oldMin + time
+    
+    if newMin < 10:
+        newMinStr = ':0' + str(newMin)
+        tempTimeStamp = part1 + part2 + newMinStr + part4
+        return tempTimeStamp
+    elif newMin > 9 and newMin < 60:
+        newMinStr = ':' + str(newMin)
+        tempTimeStamp = part1 + part2 + newMinStr + part4
+        return tempTimeStamp
+    elif newMin > 59:
+        newMinStr = ':0' + str(newMin - 60)
+        newHr = int(part2) + 1
+        if newHr < 10:
+            newHrStr = '0' + str(newHr)
+        else:
+            newHrStr = str(newHr)
+        tempTimeStamp = part1 + newHrStr + newMinStr + part4
+        return tempTimeStamp
+
 ##Prints out the time stamp and the data from station_id input
-count = 0
-station_value = ""
-station_id = ""
-date = ""
-first_timestamp = "YYYY-MM-DDT00:00:00+08:00"
-last_timestamp = "23:59:00"
+
+
+
 date = input("Please indicate the date that you want to draw the wind direction data from(YYYY-MM-DD): ")
 url = url + date
 
+# puts the data into a variable
 data = requests.get(url).json()
 for item in data['metadata']['stations']:
     print('ID is ' + str(item['id']) + '. It is located at ' + item['name'])
@@ -46,24 +66,49 @@ timeStampCount = 0
 timeStampNoCount = 0
 with open(fileName,'w') as myFile:
     for item in data['items']:
-        timeStamp = item['timestamp']
-        currentTimeStamp = timeConvert(timeStamp[11:16])
-        previousTimeStamp = timeConvert(previous_timestamp[11:16])
-        timeDiff = currentTimeStamp - previousTimeStamp
-        if timeDiff > 1:
-            timeStampNoCount = timeStampNoCount + timeDiff - 1
-        readings = item['readings']
-        for item in readings:
-            if item['station_id'] == station_id:
-                station_value = str(item['value'])
-        if station_value == "":
-            station_value = 0
+        if count == 0:
+            timeStamp = first_timestamp
+            station_value = item['value']
+            myFile.write(timeStamp + ";" + station_id)
             count = count + 1
-        # print("At {}, readings at {} = {}".format(timeStamp, station_id, station_value))
-        myFile.write("At {}, readings at {} = {}\n".format(timeStamp, station_id, station_value))
-        previous_timestamp = timeStamp
-        timeStampCount = timeStampCount + 1
-        timeDiff = 0
+
+        if count > 0:
+            timeStamp = item['timeStamp']
+            station_value = item['value']
+            currentTimeStamp = timeConvert(timeStamp[11:16])
+            previousTimeStamp = timeConvert(previous_timestamp[11:16])
+            timeDiff = currentTimeStamp - previousTimeStamp
+            station_value = str(item['value'])
+            if timeDiff > 1:
+                timeStampNoCount = timeStampNoCount + timeDiff - 1
+                for time in range(1,timeDiff):
+                    tempTimeStamp = newTimeStamp(previous_timestamp, time)
+                    print("At {}, readings at {} = {}\n".format(tempTimeStamp, station_id, station_value))
+            
+            print("At {}, readings at {} = {}\n".format(timeStamp, station_id, station_value))
+            previous_timestamp = timeStamp
+            timeStampCount = timeStampCount + 1
+            timeDiff = 0
+
+            timeStamp = item['timestamp']
+            currentTimeStamp = timeConvert(timeStamp[11:16])
+            previousTimeStamp = timeConvert(previous_timestamp[11:16])
+            timeDiff = currentTimeStamp - previousTimeStamp
+            if timeDiff > 1:
+                timeStampNoCount = timeStampNoCount + timeDiff - 1
+
+            readings = item['readings']
+            for item in readings:
+                if item['station_id'] == station_id:
+                    station_value = str(item['value'])
+            if station_value == "":
+                station_value = 0
+                count = count + 1
+            # print("At {}, readings at {} = {}".format(timeStamp, station_id, station_value))
+            myFile.write("At {}, readings at {} = {}\n".format(timeStamp, station_id, station_value))
+            previous_timestamp = timeStamp
+            timeStampCount = timeStampCount + 1
+            timeDiff = 0
     # print('There are {} empty slots for the day.'.format(str(count)))
     timeStampNoCount = timeStampNoCount - 1 
     myFile.write('There are {} empty slots for the day.\nThere are {} lines.\nThere are {} missing lines'.format(str(count), str(timeStampCount), str(timeStampNoCount)))  
