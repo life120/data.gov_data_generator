5798480ceb80bf50465995be7776364e5b188e49
life120 <69447142+life120@users.noreply.github.com>
2021-03-26 00:06:22 +0800

    Monthly Data saves all readings into one file

--- test.py
+++ test.py
@@ -1,53 +1,106 @@
-
-import pandas as pd
-import os as os
 import json as json
-import urllib3 as urllib3
 import requests
 from calendar import monthrange
-import time
 
+def loadStationID():
+    data = open("station.json")
+    data = json.load(data)
+    for item in data['metadata']['stations']:
+        print('Station ' + item['id'] + ' is located at ' + item['name'])
 
+def timeConvert(timeStamp):
+    min = int(timeStamp[3:])
+    hr = int(timeStamp[0:2])
+    time = 60*hr + min
+    return time
 
-def generateMonthTxtFile(date,station_id):
-    count = 0
-    station_value = ""
-    station_id_value = station_id
-    date = date
-    first_timestamp = "YYYY-MM-DDT00:00:00+08:00"
-    # last_timestamp = "23:59:00"
-    missing_timestamp = []
-    url = 'https://api.data.gov.sg/v1/environment/wind-direction?date='
-    timeStampCount = 0
-    timeStampNoCount = 0
-    url = url + date
-    # puts the data into a variable
-    data = requests.get(url).json()
-    # for item in data['metadata']['stations']:
-    #     print('ID is ' + str(item['id']) + '. It is located at ' + item['name'])
-    fileName = str(date) + '_'+ station_id_value +'.txt'
-    previous_timestamp = first_timestamp
-    with open(fileName,'w') as myFile:
+def newTimeStamp(currentTimeStamp, time):
+    part1 = currentTimeStamp[:11]
+    part2 = currentTimeStamp[11:13]
+    part4 = currentTimeStamp[16:]
+    oldMin = int(currentTimeStamp[14:16])
+    newMin = oldMin + time
+    
+    if newMin < 10:
+        newMinStr = ':0' + str(newMin)
+        tempTimeStamp = part1 + part2 + newMinStr + part4
+        return tempTimeStamp
+    elif newMin > 9 and newMin < 60:
+        newMinStr = ':' + str(newMin)
+        tempTimeStamp = part1 + part2 + newMinStr + part4
+        return tempTimeStamp
+    elif newMin > 59:
+        newMinStr = ':0' + str(newMin - 60)
+        newHr = int(part2) + 1
+        if newHr < 10:
+            newHrStr = '0' + str(newHr)
+        else:
+            newHrStr = str(newHr)
+        tempTimeStamp = part1 + newHrStr + newMinStr + part4
+        return tempTimeStamp
+
+def firstTimeStamp(currentTimeStamp):
+    part1 = currentTimeStamp[:11]
+    part2 = currentTimeStamp[11:13]
+    part4 = currentTimeStamp[16:]
+    newMinStr = ':00'
+    tempTimeStamp = part1 + part2 + newMinStr + part4
+    return tempTimeStamp
+
+loadStationID()
+
+station_id = input("Please indicate the station id of interest(e.g. S109): ")
+month_input = input("Please indicate the month of interest(YYYY-MM): ")
+
+year = int(month_input[0:4])
+yearStr = str(year)
+month = int(month_input[5:])
+if month < 10:
+    monthStr = "0" + str(month)
+else:
+    monthStr = str(month)
+
+month_range = monthrange(year, month)
+total_days = month_range[1]
+count = 0
+station_value = ""
+first_timestamp = "YYYY-MM-DDT00:00:00+08:00"
+# last_timestamp = "23:59:00"
+missing_timestamp = []
+url = 'https://api.data.gov.sg/v1/environment/wind-direction?date='
+timeStampCount = 0
+timeStampNoCount = 0
+fileName = str(month_input) + '_'+ station_id +'.txt'
+with open(fileName,'w') as myFile:    
+    for i in range(total_days):
+        i = i + 1
+        if i < 10:
+            dayStr = "0" + str(i)
+        else:
+            dayStr = str(i)
+        date = yearStr + "-" + monthStr + "-" + dayStr
+        url = url + date
+        data = requests.get(url).json()
+        previous_timestamp = first_timestamp
         for item in data['items']:
-            timeStamp = item["timestamp"]
+            timeStamp = item['timestamp']
             readings = item['readings']
             if count == 0:
                 temTimeStamp = firstTimeStamp(timeStamp)
                 for item in readings:
-                    if item['station_id'] == station_id_value:
+                    if item['station_id'] == station_id:
                         station_value = str(item['value'])
                 if station_value == "":
                     station_value = str(0)
-                myFile.write(temTimeStamp + ";" + station_value + "\n")
+                myFile.write(temTimeStamp + "," + station_value + "\n")
                 count = count + 1
-
             if count > 0:
                 count = count + 1
                 currentTimeStamp = timeConvert(timeStamp[11:16])
                 previousTimeStamp = timeConvert(previous_timestamp[11:16])
                 timeDiff = currentTimeStamp - previousTimeStamp
                 for item in readings:
-                    if item['station_id'] == station_id_value:
+                    if item['station_id'] == station_id:
                         station_value = str(item['value'])
                 if station_value == "":
                     station_value = str(0)
@@ -59,46 +112,27 @@ def generateMonthTxtFile(date,station_id):
                         tempTimeStamp = newTimeStamp(previous_timestamp, time)
                         myFile.write(tempTimeStamp + ";" + station_value + "\n")
                 
-                myFile.write(timeStamp + ";" + station_value + "\n")
+                myFile.write(timeStamp + "," + station_value + "\n")
                 previous_timestamp = timeStamp
                 timeStampCount = timeStampCount + 1
                 timeDiff = 0
 
-        myFile.write('There are {} slots for the day.\nThere are {} lines.\nThere are {} missing lines'.format(str(count), str(timeStampCount), str(timeStampNoCount)))  
-        myFile.write('\n' + str(1438 - timeStampCount))
-        myFile.write('\nTimestamp missing in the list are listed below:\n')
-        for item in missing_timestamp:
-            myFile.write(item + "\n")
+        # myFile.write('There are {} slots for the day.\nThere are {} lines.\nThere are {} missing lines'.format(str(count), str(timeStampCount), str(timeStampNoCount)))  
+        # myFile.write('\n' + str(1438 - timeStampCount))
+        # myFile.write('\nTimestamp missing in the list are listed below:\n')
+        # for item in missing_timestamp:
+        #     myFile.write(item + "\n")
+        print("Finished printing date: " + date + " at station id: " + station_id)
+        url = 'https://api.data.gov.sg/v1/environment/wind-direction?date='
 
-    print('There are {} empty slots for the day.\nThere are {} lines.\nThere are {} missing lines'.format(str(count), str(timeStampCount), str(timeStampNoCount)))  
-    print('\n' + str(1438 - timeStampCount))
-    print("Completed")
-    print(missing_timestamp)
+    
 
-dataType = input("Please indicate if you want daily data or month data? (D = Daily; M = Monthly):")
+print("done")
 
-if dataType == "D":
-    date = input("Please indicate the date that you want to download the wind direction data from(YYYY-MM-DD): ")
-    generateDayTxtFile(date)
-elif dataType == "M":
-    month_input = input("Please indicate the month and year that you want to download from? (YYYY-MM): ")
-    station_id = input("Please indicate the station id: ")
-    year = int(month_input[0:4])
-    yearStr = str(year)
-    month = int(month_input[5:])
-    if month < 10:
-        monthStr = "0" + str(month)
-    else:
-        monthStr = str(month)
-    month_range = monthrange(year, month)
-    total_days = month_range[1]
-    for i in range(total_days):
-        i = i + 1
-        if i < 10:
-            dayStr = "0" + str(i)
-        else:
-            dayStr = str(i)
-        date = yearStr + "-" + monthStr + "-" + dayStr
-        generateMonthTxtFile(date,station_id)
+
+    
+
+
+
         
         
